07cdcb4f0536587aaab6926733d2fa51023afa28
helenep <helene.pruvot1@gmail.com>
2015-03-17 16:59:30 +0100

    profily + groww

--- app/assets/js/script.js
+++ app/assets/js/script.js
@@ -26,5 +26,80 @@ $(document).ready(function() {
         $(this).html($(this).text() == 'Hide grid' ? 'Show grid' : 'Hide grid');
         $(".mockup-dyskit figcaption img").toggleClass("hidden");
     });
-	
+
+
+
+  var SlideWidth = 250;
+  var SlideSpeed = 800;
+
+  function currentMargin() {
+    // get current margin of slider
+    var currentMargin = $("#slider-groww").css("margin-left");
+
+    // first page load, margin will be auto, we need to change this to 0
+    if (currentMargin == "auto") {
+        currentMargin = 0;
+    }
+
+    // return the current margin to the function as an integer
+    return parseInt(currentMargin);
+  }
+
+  function SetNavigationDisplay(direction) {
+      // get current margin
+      var currentMargin = currentMargin();
+
+      // get wrapper width
+      var wrapperWidth = $("#slider-groww").width();
+
+      if(direction == "next") {
+        NextSlide();
+      }
+      else {
+        PreviousSlide();
+      }
+  }
+
+
+  function NextSlide() {
+      // get the current margin and subtract the slide width
+      var newMargin = currentMargin() - SlideWidth-20;
+
+      // slide the wrapper to the left to show the next panel at the set speed. Then set the nav display on completion of animation.
+      $("#slider-groww").animate({ 
+        marginLeft: newMargin 
+      }, SlideSpeed);
+  }
+
+
+  function PreviousSlide() {
+      // get the current margin and subtract the slide width
+      var newMargin = currentMargin() + SlideWidth+20;
+
+      // slide the wrapper to the right to show the previous panel at the set speed. Then set the nav display on completion of animation.
+      $("#slider-groww").animate({ 
+        marginLeft: newMargin 
+      }, SlideSpeed);
+  } 
+
+  $('#slider-groww .slide').on("click",function(){ 
+    
+    if($(this).hasClass("next")) {
+      NextSlide();
+    } else if($(this).hasClass("prev")) { 
+      PreviousSlide();
+    }
+
+
+    $('.slide').removeClass("fg bg bgbg prev next");
+    $('.slide').addClass("bgbg");
+    $(this).removeClass("bgbg").addClass("fg");
+    $(this).next().removeClass("bgbg").addClass("bg next");
+    $(this).prev().removeClass("bgbg").addClass("bg prev");
+
+    var index = $(this).index();
+    $('.bullet').removeClass("active");
+    $('.bullet').eq(index).addClass("active");
+
+  })
 })
